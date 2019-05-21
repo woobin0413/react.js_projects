5c900251a291591ec75853fd5c94c256dd4af2a4
spaceboy <woobin0413@gmail.com>
2019-05-21 01:15:40 -0500

    cards , home
    reveal, show:true, false,

--- udemy_second_app/src/Components/Home/index.jsx
+++ udemy_second_app/src/Components/Home/index.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import Featured from "./Featured";
 import Matches from "./matches";
-
+import MeetPlayers from "./meetPlayers";
 const Home = () => {
   return (
     <div className="bck_blue">
       <Featured />
       <Matches />
+      <MeetPlayers />
     </div>
   );
 };
--- udemy_second_app/src/firebase.jsx
+++ udemy_second_app/src/firebase.jsx
@@ -3,13 +3,13 @@ import "firebase/app";
 import "firebase/database";
 
 var config = {
-  apiKey: "apiKey",
-  authDomain: "authDomain.firebaseapp.com",
-  databaseURL: "https://databaseURL.firebaseio.com",
-  projectId: "projectId",
-  storageBucket: "storageBucket",
-  messagingSenderId: "messagingSenderId",
-  appId: "appId"
+  apiKey: "AIzaSyBv1Ypss6bzHsedhN0lmewu0qhfLC-84l0",
+  authDomain: "m-city-32017.firebaseapp.com",
+  databaseURL: "https://m-city-32017.firebaseio.com",
+  projectId: "m-city-32017",
+  storageBucket: "m-city-32017.appspot.com",
+  messagingSenderId: "545952474114",
+  appId: "1:545952474114:web:ff0a293c8ed4859e"
 };
 
 firebase.initializeApp(config);
