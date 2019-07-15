c0c6738b294d2806782b981c712a8b07572d619a
spaceboy <woobin0413@gmail.com>
2019-07-14 19:16:45 -0500

    ,
    ,

--- udemy_second_app/src/firebase.js
+++ udemy_second_app/src/firebase.js
@@ -5,13 +5,13 @@ import "firebase/auth";
 import "firebase/storage";
 
 const config = {
-  apiKey: "AIzaSyBv1Ypss6bzHsedhN0lmewu0qhfLC-84l0",
-  authDomain: "m-city-32017.firebaseapp.com",
-  databaseURL: "https://m-city-32017.firebaseio.com",
-  projectId: "m-city-32017",
-  storageBucket: "m-city-32017.appspot.com",
-  messagingSenderId: "545952474114",
-  appId: "1:545952474114:web:ff0a293c8ed4859e"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 firebase.initializeApp(config);
